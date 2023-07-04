e8cce6f3a0330f308f8235070142e3f58321d7aa
Ph0enixKM <pkaras.it@gmail.com>
2023-07-04 19:29:56 +0200

    Initial Commit

---
+++ CodeCut/CodeCutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeCutApp.swift
+//  CodeCut
+//
+//  Created by Paweł Karaś on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CodeCutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
